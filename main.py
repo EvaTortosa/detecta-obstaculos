7b776d2b4060dd61a5b968ee631d3cd87a7fc83a
EvaTortosa <48592760+EvaTortosa@users.noreply.github.com>
2022-05-25 18:35:06 +0200

    Actualizar pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+def on_button_pressed_a():
+    global boton, idioma
+    boton = 1
+    idioma = 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global boton, idioma
+    boton = 1
+    idioma = 2
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+idioma = 0
+boton = 0
 basic.clear_screen()
+cuentapersonas = 0
+boton = 0
+idioma = 0
+nuevoespectador = 1
 
 def on_forever():
+    global cuentapersonas, nuevoespectador, boton
     if pins.digital_read_pin(DigitalPin.P0) == 1:
-        basic.pause(100)
+        basic.pause(1000)
         if pins.digital_read_pin(DigitalPin.P0) == 1:
+            if nuevoespectador == 1:
+                cuentapersonas += 1
+                nuevoespectador = 0
             # Reproducir audio:
             # Bienvenido al teatro de
             # Alborán smart city.
@@ -17,8 +39,31 @@ def on_forever():
                                 # . . . #
                                 . # # # .
             """)
+            while boton == 1:
+                boton = 0
+                if idioma == 1:
+                    # Reproducir video obra en español
+                    basic.show_leds("""
+                        . # . # .
+                                                # # # # #
+                                                # # # # #
+                                                # # # # #
+                                                . # # # .
+                    """)
+                    nuevoespectador = 1
+                elif idioma == 2:
+                    # Reproducir video obra en inglés
+                    basic.show_leds("""
+                        . # . # .
+                                                . # . # .
+                                                # # # # #
+                                                # . . . #
+                                                . . . . .
+                    """)
+                    nuevoespectador = 1
         else:
             basic.show_icon(IconNames.ASLEEP)
     else:
         basic.show_icon(IconNames.ASLEEP)
+    serial.write_value("cuentapersonas", cuentapersonas)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     boton = 1
     idioma = 1
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     boton = 1
     idioma = 2
 })
@@ -13,7 +15,8 @@ let cuentapersonas = 0
 boton = 0
 idioma = 0
 let nuevoespectador = 1
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         basic.pause(1000)
         if (pins.digitalReadPin(DigitalPin.P0) == 1) {
@@ -21,58 +24,52 @@ basic.forever(function () {
                 cuentapersonas += 1
                 nuevoespectador = 0
             }
-            // Reproducir audio:
-            // Bienvenido al teatro de
-            // Alborán smart city.
-            // pulse botón A para rescuchar la obra en español.
-            // pulse botón B para escuchar la obra en inglés
-            // Wellcome to .....
+            
+            //  Reproducir audio:
+            //  Bienvenido al teatro de
+            //  Alborán smart city.
+            //  pulse botón A para rescuchar la obra en español.
+            //  pulse botón B para escuchar la obra en inglés
+            //  Wellcome to .....
             basic.showLeds(`
                 . # . # .
-                . . . . .
-                # # # # #
-                # . . . #
-                . # # # .
-                `)
+                                . . . . .
+                                # # # # #
+                                # . . . #
+                                . # # # .
+            `)
             while (boton == 1) {
                 boton = 0
                 if (idioma == 1) {
-                    // Reproducir audio:
-                    // Bienvenido al teatro de
-                    // Alborán smart city.
-                    // pulse botón A para rescuchar la obra en español.
-                    // pulse botón B para escuchar la obra en inglés
-                    // Wellcome to .....
+                    //  Reproducir video obra en español
                     basic.showLeds(`
                         . # . # .
-                        # # # # #
-                        # # # # #
-                        # # # # #
-                        . # # # .
-                        `)
+                                                # # # # #
+                                                # # # # #
+                                                # # # # #
+                                                . # # # .
+                    `)
                     nuevoespectador = 1
                 } else if (idioma == 2) {
-                    // Reproducir audio:
-                    // Bienvenido al teatro de
-                    // Alborán smart city.
-                    // pulse botón A para rescuchar la obra en español.
-                    // pulse botón B para escuchar la obra en inglés
-                    // Wellcome to .....
+                    //  Reproducir video obra en inglés
                     basic.showLeds(`
                         . # . # .
-                        . # . # .
-                        # # # # #
-                        # . . . #
-                        . . . . .
-                        `)
+                                                . # . # .
+                                                # # # # #
+                                                # . . . #
+                                                . . . . .
+                    `)
                     nuevoespectador = 1
                 }
+                
             }
         } else {
             basic.showIcon(IconNames.Asleep)
         }
+        
     } else {
         basic.showIcon(IconNames.Asleep)
     }
+    
     serial.writeValue("cuentapersonas", cuentapersonas)
 })
